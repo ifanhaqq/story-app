c553ae443bbd39e8ffa35a032c395a68b0c1db62
Hanifan Haqin <114238553+ifanhaqq@users.noreply.github.com>
2025-04-04 01:16:20 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
   plugins: [
     VitePWA({
       strategies: "injectManifest", // Use your custom SW file
