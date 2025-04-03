68a08a210d04b40d949b8a65e918eedf743e5a3a
ifanhaqq <haqinhanifan@gmail.com>
2025-04-04 01:37:39 +0700

    main for development

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/story-app/' : '/',
   plugins: [
     VitePWA({
       strategies: "injectManifest", // Use your custom SW file
